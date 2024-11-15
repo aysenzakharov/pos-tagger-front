84b7fdc32a78a9433ba490516e7fbeccc6b8eaff
Aysen Zakharov <zeinell69@gmail.com>
2024-11-15 20:57:17 +0300

    api path upd

--- src/backend.ts
+++ src/backend.ts
@@ -12,7 +12,7 @@ export async function getTags(text: string): Promise<Tag[]> {
     };
 
     try {
-        const response = await fetch("https://part-of-speech-tool.info/pos_tagging/", requestOptions);
+        const response = await fetch("https://part-of-speech-tool.info/api/v1/pos_tagging/", requestOptions);
         let data = await response.json();
         return data.pos_tags
     } catch (error) {
