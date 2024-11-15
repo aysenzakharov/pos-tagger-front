6ca1351d6c2d36f1930bca882bb8ea32b0d5d999
Aysen Zakharov <zeinell69@gmail.com>
2024-11-15 03:36:11 +0300

    first version

---
+++ src/backend.ts
@@ -0,0 +1,22 @@
+export type Tag = [word: string, pos: string]
+
+export async function getTags(text: string): Promise<Tag[]> {
+    const myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+    const raw = JSON.stringify({ "text": text });
+    const requestOptions: RequestInit = {
+        method: "POST",
+        headers: myHeaders,
+        body: raw,
+        redirect: "follow"
+    };
+
+    try {
+        const response = await fetch("https://part-of-speech-tool.info/pos_tagging/", requestOptions);
+        let data = await response.json();
+        return data.pos_tags
+    } catch (error) {
+        console.error('Ошибка при получении POS-тегов:', error);
+        return [];
+    }
+}
