5b7241b61424843bc4a04f072cbf0000a1afbfa3
Aysen Zakharov <zeinell69@gmail.com>
2025-06-16 19:24:24 +0300

    deploy fix

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 yarn build
-ssh vps@nsk-group.shop "rm -rf /home/vps/pos-tagger-front/build/*"
-rsync -av --progress build/ vps@nsk-group.shop:/home/vps/pos-tagger-front/build
+ssh vps@part-of-speech-tool.info "rm -rf /home/vps/pos-tagger-front/build/*"
+rsync -av --progress build/ vps@part-of-speech-tool.info:/home/vps/pos-tagger-front/build
